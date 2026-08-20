db8a679a08f711e546aa0022a45e6b2adae07bae
MOPELotus <79179977+MOPELotus@users.noreply.github.com>
2026-08-20 23:31:11 +0800

    chore: bump Night Node to 1.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.lotus.lanmihome.client"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.6.0"
+        versionCode = 12
+        versionName = "1.6.1"
     }
 
     flavorDimensions += "role"
