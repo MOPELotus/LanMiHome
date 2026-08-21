635cb276f8a8b6a9dac0a6bbe4020534f583aa09
MOPELotus <79179977+MOPELotus@users.noreply.github.com>
2026-08-22 01:13:03 +0800

    Add W96D capability descriptor

---
+++ app/src/main/java/dev/lotus/lanmihome/W96DCapability.kt
@@ -0,0 +1,15 @@
+package dev.lotus.lanmihome
+
+object W96DCapability {
+    val features = listOf(
+        "power",
+        "speed_0_100",
+        "natural_wind",
+        "turbo",
+        "indicator_light",
+        "battery_telemetry",
+        "vbus_telemetry",
+        "motor_status",
+        "ble_owner",
+    )
+}
